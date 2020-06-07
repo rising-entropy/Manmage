63953d1caac4707f0e8aed9950bfed3be6f2d07b
vaishnavibelgamwar <vaishnavi82001@gmail.com>
2020-06-07 15:45:09 +0530

    second time added

---
+++ admin mode/first page/without graphics.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{	clrscr();
+	textcolor(LIGHTGREEN);
+	cprintf("\n--------------------------------------------------------------------------------");
+	cprintf("\n\n\nWELCOME TO...\n");
+	cprintf("\n\n\nATTENDENCE MANAGER");
+	cprintf("\n\n\n\n\n\n\n PROJECT BY:ONKAR GAVALI.");
+	cprintf("\n\b\b\b\b\b\b\b\b\b\b\b\b\bDEVANG KAMBLE.");
+	cprintf("\n\b\b\b\b\b\b\b\b\b\b\b\b\b\bVAISHNAVI BELGAMWAR.                                                 ");
+	cprintf("\nMENTORS:NISHI BHATE.                                                                     ");
+	cprintf("\bABHISHEK MORE.                          ");
+	cprintf("--------------------------------------------------------------------------------");
+	delay(100);
+	getch();
+	return;
+}
